3403a1c4a191a8338c31d23e36b6a5693186c7c7
Lioncat6 <bwhouseholder2008@gmail.com>
2023-11-29 19:06:07 -0600

    Updated main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+#######################
+# ChomebookMegascript
+#
+# This script is a combination of other scripts
+# that i've found in various parts of the web 
+# and will be properly credited in their respective spots.
+#  
+# Disclaimer: 
+# I am not responsible to any damage or bricking of your device
+# By using this script, you acknowledge this.
+#
+# Created by Lioncat6:
+# https://github.com/Lioncat6
+#
+#######################spell
+#Set some variables to get this ball rolling
+
+CBMS_VERSION='0.0.1'
+CBMS_DATE='11/29/23'
+
+#check for internet
