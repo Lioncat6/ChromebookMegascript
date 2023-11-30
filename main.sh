6cff06ec8ecbfe79f316f491826cc6ee3ab460f6
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2023-11-29 19:09:50 -0600

    Internet checking

--- main.sh
+++ main.sh
@@ -3,11 +3,11 @@
 # ChomebookMegascript
 #
 # This script is a combination of other scripts
-# that i've found in various parts of the web 
+# that I've found in various parts of the web 
 # and will be properly credited in their respective spots.
 #  
 # Disclaimer: 
-# I am not responsible to any damage or bricking of your device
+# I am not responsible for any damage or bricking of your device
 # By using this script, you acknowledge this.
 #
 # Created by Lioncat6:
@@ -20,3 +20,11 @@ CBMS_VERSION='0.0.1'
 CBMS_DATE='11/29/23'
 
 #check for internet
+echo -e "GET http://github.com HTTP/1.0\n\n" | nc github.com 80 > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    true
+else
+    echo "Cannot establish a connection to GitHub! Aborting..."
+    exit 0
+fi
